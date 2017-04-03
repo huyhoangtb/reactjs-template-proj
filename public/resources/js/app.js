df752d568367846a3ec3fbcb8ad3de47b87fe89f
Peter Hoang Nguyen <vntopmas@gmail.com>
2017-04-03 10:18:36 +0700

    commit first version

---
+++ public/resources/js/app.js
@@ -0,0 +1,10 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"D:\\projects\\hoangnh\\reactjs-template-proj\\src\\index.js\""); }());
+
+
+/***/ }
+]);
+//# sourceMappingURL=app.js.map
